6b4a9e6cf34a95a714215846e91cc0e75e5df913
dlsso <supertonesfan@hotmail.com>
2014-07-03 12:51:05 -0600

    Added first week

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
+
+
 $(document).on('ready', function() {
-  
+	
+	for (var i = 0; i < 7; i++) {
+		$('#content').append("<h2 class='day'>" + moment().add('days', i).format('LL') + "</h2>")
+	};
 });
