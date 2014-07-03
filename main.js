5b57a46aa85e715ec4cc357a1df9362ae1025d88
dlsso <supertonesfan@hotmail.com>
2014-07-03 13:15:25 -0600

    Made add appointment buttons

--- main.js
+++ main.js
@@ -4,6 +4,11 @@
 $(document).on('ready', function() {
 	
 	for (var i = 0; i < 7; i++) {
-		$('#content').append("<h2 class='day'>" + moment().add('days', i).format('LL') + "</h2>")
-	};
+		$('#content').append("<div class='day'><h2>" + moment().add('days', i).format('LL') + "</h2><button class='add-button'>Add appointment</button></div>")
+		}
+
+		$('.add-button').on('click', function(){
+			$(this).closest('div').append("<h3> hi</h3>")
+		});
+
 });
